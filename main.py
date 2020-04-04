45e1c2f7c689d6f313a7016e82a6ce1a1ab55fed
Ivan <oiv@live.ru>
2020-04-04 21:40:49 +0500

    v. 0.2
    XML работает

--- main.py
+++ main.py
@@ -31,13 +31,37 @@ with open('newsafr.json') as f:
     for item in list_of_word_counts[:10]:
         print(f'{item[0]} - {item[1]}')
 
-# print('10 самых часто встречающихся слов из файла .xml:')
-#
-# import xml.etree.ElementTree as ET
-#
-# with open('newsafr.xml') as f:
-#     xml_text = f.read()
-#     xml_root = ET.fromstring(xml_text)
-#     print(xml_root.attrib)
+print('\n 10 самых часто встречающихся слов из файла .xml:')
 
+import xml.etree.ElementTree as ET
 
+with open('newsafr.xml') as f:
+    parser = ET.XMLParser(encoding="utf-8")
+    tree = ET.parse(f, parser=parser)
+    root = tree.getroot()
+    channel = root.find('channel')
+    items = channel.findall('item')
+
+    all_list = []
+
+    for item in items:
+        all_list.append(item.find('description').text.split(' '))
+
+    word_list = []
+    for item in all_list:
+        for word in item:
+            word_list.append(word)
+
+    word_list = [x for x in word_list if len(x) >= 6]
+
+    word_list_unique = list(set(word_list))
+
+    word_counts = {}
+
+    for word in word_list_unique:
+        word_counts[word] = word_list.count(word)
+
+    list_of_word_counts = sorted(word_counts.items(), reverse = True, key = lambda item: item[1])
+
+    for item in list_of_word_counts[:10]:
+        print(f'{item[0]} - {item[1]}')
